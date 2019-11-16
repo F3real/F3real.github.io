3600db3dfe2d6da5070d49c383127afcae7189e5
F3real <stefan92ff@yandex.com>
2019-11-17 00:45:21 +0100

    Additonally automate github publishing

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,13 @@
-xcopy /s /Y .\output . && git add . && git checkout --orphan t && git add -u && git commit -m "Regenerate site" && call git branch -D master && call git branch -m master && git push -f origin masterbranch -D master && call git branch -m master && git push -f origin master
+git apply github.patch && ^
+invoke build && ^
+git reset --hard && ^
+git checkout master && ^
+xcopy /s /Y .\output . && ^
+git add . && ^
+git checkout --orphan t && ^
+git add -u && ^
+git commit -m "Regenerate site" && ^
+call git branch -D master && ^
+call git branch -m master && ^
+git push -f origin master && ^
+git checkout dev
